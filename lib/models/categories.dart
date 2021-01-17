e123cac789f97092355d6ace41f995a6e364ca8b
Your Name <hossamomar9876@gmail.com>
2021-01-17 14:03:53 +0200

    reinit app

---
+++ lib/models/categories.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class Categories {
+  final String id;
+  final String title;
+  final String imageUrl;
+  final Color color1;
+  final Color color2;
+
+  Categories({
+    @required this.id,
+    @required this.title,
+    @required this.imageUrl,
+    @required this.color1,
+    @required this.color2,
+  });
+}
